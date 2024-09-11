a9c83a3c1c4d6cd440eae6bddc4a209d9b8ec91c
João Vitor <joaos_vb@hotmail.com>
2024-09-11 19:32:44 -0300

    primeiro commit

---
+++ src/http/routes/getPendingGoals.ts
@@ -0,0 +1,10 @@
+import type { FastifyPluginAsyncZod } from 'fastify-type-provider-zod'
+import { getWeekPendingGoals } from '../../services/getWeekPendingGoal'
+
+export const getPendingGoalsRoute: FastifyPluginAsyncZod = async app => {
+  app.get('/pendingGoals', async () => {
+    const { pendingGoals } = await getWeekPendingGoals()
+
+    return pendingGoals
+  })
+}
---
+++ src/http/server.ts
@@ -0,0 +1,33 @@
+import fastify from 'fastify'
+import {
+  serializerCompiler,
+  validatorCompiler,
+  type ZodTypeProvider,
+} from 'fastify-type-provider-zod'
+import { createGoalRoute } from './routes/createGoal'
+import { createCompletionRoute } from './routes/createCompletion'
+import { getPendingGoalsRoute } from './routes/getPendingGoals'
+import { getWeekSummaryRoute } from './routes/getWeekSummary'
+import fastifyCors from '@fastify/cors'
+
+const app = fastify().withTypeProvider<ZodTypeProvider>()
+
+app.register(fastifyCors, {
+  origin: '*',
+})
+
+app.setValidatorCompiler(validatorCompiler)
+app.setSerializerCompiler(serializerCompiler)
+
+app.register(createGoalRoute)
+app.register(createCompletionRoute)
+app.register(getPendingGoalsRoute)
+app.register(getWeekSummaryRoute)
+
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log('HTTP server running!')
+  })
